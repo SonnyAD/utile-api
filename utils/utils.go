cf7f66bdd7e39a993d516bed9fc05d91eabddeb4
SonnyAD <sonny@alvesdi.as>
2024-08-13 23:55:30 +0800

    Adds missing forwarding in middleware

--- utils/utils.go
+++ utils/utils.go
@@ -12,6 +12,8 @@ import (
 func EnableCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		next.ServeHTTP(w, r)
 	})
 }
 
